84e8304ad54b808a117174d0049f65f4d7920a83
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-07-10 19:17:40 +0800

    tailwind cdn added

--- resources/views/auth/layout.blade.php
+++ resources/views/auth/layout.blade.php
@@ -5,10 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CashTrack - Track Manage Thrive</title>
-    
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
     @yield('content')
-
 </body>
 </html>
