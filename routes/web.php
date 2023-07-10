a2a9ed0ec31dbbfa792c1171f5f8e9fc6333d7e5
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-07-10 19:18:10 +0800

    typo fixed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Contrrollers\Auth\AuthenticationController;
+use App\Http\Controllers\Auth\AuthenticationController;
 
 /*
 |--------------------------------------------------------------------------
